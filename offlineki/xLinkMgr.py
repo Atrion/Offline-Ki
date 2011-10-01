7033fafa7003872cb1d62a1016b263c7f6ddeb30
diafero <diafero@arcor.de>
2011-10-01 18:22:07 +0200

    clarify that the code may also be used under GPL without any exception (which IMHO was already the case before)

--- offlineki/xLinkMgr.py
+++ offlineki/xLinkMgr.py
@@ -9,7 +9,8 @@
 #    This program is free software; you can redistribute it and/or modify      #
 #    it under the terms of the GNU General Public License as published by      #
 #    the Free Software Foundation; either version 2 of the License, or         #
-#    (at your option) any later version, with the Uru exception (see below).   #
+#    (at your option) any later version, with or (at your option) without      #
+#    the Uru exception (see below).                                            #
 #                                                                              #
 #    This program is distributed in the hope that it will be useful,           #
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
@@ -20,9 +21,10 @@
 #    along with this program; if not, write to the Free Software               #
 #    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA #
 #                                                                              #
-#    Please see the file COPYING for the full GPLv2 license. In addition,      #
-#    this file may be used in combination with (non-GPL) code                  #
-#    within the context of Uru.                                                #
+#    Please see the file COPYING for the full GPLv2 license.                   #
+#                                                                              #
+#    Uru exception: In addition, this file may be used in combination with     #
+#    (non-GPL) code within the context of Uru.                                 #
 #                                                                              #
 #==============================================================================#
 import string
