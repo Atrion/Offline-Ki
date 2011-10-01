6ebf50d6ad080778f2368a8a6f5d9b857f006c8f
diafero <diafero@arcor.de>
2011-10-01 18:58:13 +0200

    license change from GPLv2+ to GPLv3+

--- offlineki/xChronHandler.py
+++ offlineki/xChronHandler.py
@@ -4,11 +4,12 @@
 #    Offline KI                                                                #
 #                                                                              #
 #    Copyright (C) 2004-2011  The Offline KI contributors                      #
-#    See the file AUTHORS for more info about the contributors                 #
+#    See the file AUTHORS for more info about the contributors (including      #
+#    contact information)                                                      #
 #                                                                              #
-#    This program is free software; you can redistribute it and/or modify      #
+#    This program is free software: you can redistribute it and/or modify      #
 #    it under the terms of the GNU General Public License as published by      #
-#    the Free Software Foundation; either version 2 of the License, or         #
+#    the Free Software Foundation, either version 3 of the License, or         #
 #    (at your option) any later version, with or (at your option) without      #
 #    the Uru exception (see below).                                            #
 #                                                                              #
@@ -17,11 +18,8 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
 #    GNU General Public License for more details.                              #
 #                                                                              #
-#    You should have received a copy of the GNU General Public License         #
-#    along with this program; if not, write to the Free Software               #
-#    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA #
-#                                                                              #
-#    Please see the file COPYING for the full GPLv2 license.                   #
+#    Please see the file COPYING for the full GPLv3 license, or see            #
+#    <http://www.gnu.org/licenses/>                                            #
 #                                                                              #
 #    Uru exception: In addition, this file may be used in combination with     #
 #    (non-GPL) code within the context of Uru.                                 #
