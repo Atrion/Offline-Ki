36046df996ba1029edfaa60bb8dbd3ccd7ade87b
diafero <diafero@arcor.de>
2011-10-08 17:18:49 +0200

    Sirius agreed to license these files as GPLv3+, thanks!

---
+++ offlineki/grsnClimbTypes.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#==============================================================================#
+#                                                                              #
+#    Offline KI                                                                #
+#                                                                              #
+#    Copyright (C) 2004-2011  The Offline KI contributors                      #
+#    See the file AUTHORS for more info about the contributors (including      #
+#    contact information)                                                      #
+#                                                                              #
+#    This program is free software: you can redistribute it and/or modify      #
+#    it under the terms of the GNU General Public License as published by      #
+#    the Free Software Foundation, either version 3 of the License, or         #
+#    (at your option) any later version, with or (at your option) without      #
+#    the Uru exception (see below).                                            #
+#                                                                              #
+#    This program is distributed in the hope that it will be useful,           #
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU General Public License for more details.                              #
+#                                                                              #
+#    Please see the file COPYING for the full GPLv3 license, or see            #
+#    <http://www.gnu.org/licenses/>                                            #
+#                                                                              #
+#    Uru exception: In addition, this file may be used in combination with     #
+#    (non-GPL) code within the context of Uru.                                 #
+#                                                                              #
+#==============================================================================#
+class ClimbAction:
+    """Possible actions when climbing
+This does not mean only climbing, but might also be used by regions, that's why they can all be added (except climb)."""
+    def __init__(self):
+        pass
+
+    probe    = 0
+    mount    = 1
+    dismount = 2
+    fallOff  = 4
+    release  = 8
+    idle     = 16
+
+
+class ClimbDirection:
+    """Possible directions when climbing (x, y, -x, -y)
+Also used by regions, that's why it can be added again."""
+    def __init__(self):
+        pass
+
+    up		= 1
+    down	= 2
+    left	= 4
+    right   = 8
--- offlineki/grsnWallClimb.py
+++ offlineki/grsnWallClimb.py
@@ -2,13 +2,27 @@
 #==============================================================================#
 #                                                                              #
 #    Offline KI                                                                #
-#    See the file AUTHORS for more info about the contributors.                #
+#                                                                              #
+#    Copyright (C) 2004-2011  The Offline KI contributors                      #
+#    See the file AUTHORS for more info about the contributors (including      #
+#    contact information)                                                      #
+#                                                                              #
+#    This program is free software: you can redistribute it and/or modify      #
+#    it under the terms of the GNU General Public License as published by      #
+#    the Free Software Foundation, either version 3 of the License, or         #
+#    (at your option) any later version, with or (at your option) without      #
+#    the Uru exception (see below).                                            #
 #                                                                              #
 #    This program is distributed in the hope that it will be useful,           #
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of            #
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                      #
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             #
+#    GNU General Public License for more details.                              #
 #                                                                              #
-#    You may re-use the code in this file within the context of Uru.           #
+#    Please see the file COPYING for the full GPLv3 license, or see            #
+#    <http://www.gnu.org/licenses/>                                            #
+#                                                                              #
+#    Uru exception: In addition, this file may be used in combination with     #
+#    (non-GPL) code within the context of Uru.                                 #
 #                                                                              #
 #==============================================================================#
 ### GLOBALS
