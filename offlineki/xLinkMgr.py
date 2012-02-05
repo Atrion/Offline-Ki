08a6a38aabe4eaee80010b2fa515e1b685aa3d5a
diafero <diafero@arcor.de>
2012-02-05 15:49:54 +0100

    avoid deep recursion while reading the age list, and move some operations into the age datastructure

--- offlineki/xLinkMgr.py
+++ offlineki/xLinkMgr.py
@@ -39,8 +39,10 @@ _AvailableLinks = {}
 _PublicLinks = []
 _RestorationLinks = []
 
+# Age datastructure and its operations
 class _Age:
-    def __init__(self, displayName, detect = 'dataserver', linkrule = 'basic', defaultSpawnpoint = 'LinkInPointDefault'):
+    def __init__(self, filename, displayName, detect = 'dataserver', linkrule = 'basic', defaultSpawnpoint = 'LinkInPointDefault'):
+        self.filename = filename
         self.displayName = displayName
         self.detect = detect
         self.linkrule = linkrule
@@ -48,19 +50,96 @@ class _Age:
         self.spawnpoints = {}
         self.description = ''
 
+    def IsAvailable(self):
+        if self.detect == 'disabled':
+            return xxConfig.isOffline() and os.path.exists('dat\\%s.age' % self.filename)
+        if self.detect == 'file' or (self.detect.startswith('dataserver') and xxConfig.isOffline()):
+            return os.path.exists('dat\\%s.age' % self.filename)
+        if self.detect.startswith('dataserver'): # online, obviously
+            return (self.detect == 'dataserver') or (self.detect == ('dataserver('+xxConfig.shardIdentifier+')'))
+        return False
 
-def _AddAge(linktype, ageName, age):
+    def IsBasicLinkAge(self):
+        return (self.linkrule == 'basic')
+
+    def IsOriginalBookAge(self):
+        return (self.linkrule == 'original')
+
+    # If the spawnpoint is None, use the default one. If needFullInfo is True, we need the full UUID for the age.
+    def GetAgeLinkStruct(self, spawnpoint = None, needFullInfo = False):
+        # check general age availability
+        if not self.IsAvailable():
+            return 'The age with filename \"%s\" is not available on your PC or on this Shard.' % self.filename
+        # get correct spawn point
+        if spawnpoint == None: spawnpoint = self.defaultSpawnpoint
+        # check subage
+        partnerAge = IsSubAge(PtGetAgeName()) # the name of the parent age of the current age
+        if partnerAge == self.filename:
+            print "xLinkMgr: Auto-detected %s (current age) to be a sub-age of %s" % (PtGetAgeName(), self.filename)
+        else: # the current age is not a sub age of another age, so ignore that
+            partnerAge = ''
+        if not len(partnerAge): # ok, we are not linking to our parent age, but maybe we link to a sub age of ourselves?
+            # check whether the destination age has a partner age
+            partnerAge = IsSubAge(self.filename)
+            if len(partnerAge) and partnerAge != PtGetAgeName(): # indeed we are linking to a sub age, but it's someone else's sub age!
+                return 'You can not link to %s from here.' % self.displayName
+        # intialize basic age link struct
+        als = ptAgeLinkStruct()
+        ainfo = ptAgeInfoStruct()
+        ainfo.setAgeFilename(self.filename)
+        ainfo.setAgeInstanceName(self.displayName)
+        als.setAgeInfo(ainfo)
+        # check spawnpoint and linking rule
+        if (len(partnerAge) or self.IsOriginalBookAge()): # we need to verify the spawn point
+            # we might need some information from the vault
+            vault = ptVault()
+            vinfo = ptAgeInfoStruct()
+            vinfo.setAgeFilename(self.filename)
+            vinfo = vault.getOwnedAgeLink(vinfo)
+            if len(partnerAge):
+                # either we are linking to a sub age and checked that this is indeed a sub age of the current age
+                # or fromSubAge is true, and we checked that the current age is indeed a subage of the destination age
+                if needFullInfo: return "Can't link to a sub-age with full info"
+                als.setLinkingRules(PtLinkingRules.kSubAgeBook)
+            else: # must be an original age
+                if needFullInfo:
+                    if vinfo == None: return "Can't link to a not-yet registered owned age with full info"
+                    als = vinfo.asAgeLinkStruct()
+                als.setLinkingRules(PtLinkingRules.kOriginalBook)
+            # now, do the spawn point thingy
+            if spawnpoint in self.spawnpoints: # we got a spawn point :)
+                als.setSpawnPoint(ptSpawnPointInfo(self.spawnpoints[spawnpoint], spawnpoint))
+                return als # done!
+            # no spawn point specified in the linking definitions
+            # let's check if that age was already linked to, and the spawn point is registered in the vault
+            if vinfo != None:
+                spoints = vinfo.getSpawnPoints()
+                for spoint in spoints:
+                    if spoint.getName().lower() == spawnpoint.lower():
+                        als.setSpawnPoint(spoint) # yes, we can use it!
+                        return als # done :)
+            return 'Linking to spawn point %s in %s is not possible' % (spawnpoint, self.displayName) # no luck :(
+        elif self.IsBasicLinkAge(): # any spawn point can be used
+            als.setLinkingRules(PtLinkingRules.kBasicLink)
+            als.setSpawnPoint(ptSpawnPointInfo('LinkMgrBasicLink', spawnpoint))
+            return als
+        else:
+            return "ERROR: unable to get linkrule: %s (%s)"  % (self.filename, self.linkrule)
+
+
+# Code for collecting available ages
+def _AddAge(linktype, age):
     global _AvailableLinks, _RestorationLinks, _PublicLinks
-    #print ('xLinkMgr: Found %s to %s' % (linktype, ageName))
-    # update Nexus link lists
+    #print ('xLinkMgr: Found %s to %s' % (linktype, age.filename))
+    # update Nexus link lists (iff the age is actually available)
     if linktype == 'restorationlink':
-        if IsAgeAvailable(ageName): _RestorationLinks.append([ageName, age.displayName])
+        if age.IsAvailable(): _RestorationLinks.append([age.filename, age.displayName])
     elif linktype == 'publiclink':
-        if IsAgeAvailable(ageName): _PublicLinks.append([ageName, age.displayName])
+        if age.IsAvailable(): _PublicLinks.append([age.filename, age.displayName])
     elif linktype != 'link': # unknown link type...
         raise Exception("Unknown link type "+linktype)
     # and add the age (do this afterwards, so if the link type is invalid, you can not link here, which I will probably notice)
-    _AvailableLinks[ageName] = age
+    _AvailableLinks[age.filename] = age
 
 
 def _LoadAvailableLinks():
@@ -96,13 +175,13 @@ def _LoadAvailableLinksFile(filename):
             # read link
             elif type.endswith('link'):
                 if len(data) == 5:
-                    _AddAge(type, data[0], _Age(data[1], data[2], data[3], data[4]))
+                    _AddAge(type, _Age(data[0], data[1], data[2], data[3], data[4]))
                 elif len(data) == 4:
-                    _AddAge(type, data[0], _Age(data[1], data[2], data[3]))
+                    _AddAge(type, _Age(data[0], data[1], data[2], data[3]))
                 elif len(data) == 3:
-                    _AddAge(type, data[0], _Age(data[1], data[2]))
+                    _AddAge(type, _Age(data[0], data[1], data[2]))
                 elif len(data) == 2:
-                    _AddAge(type, data[0], _Age(data[1]))
+                    _AddAge(type, _Age(data[0], data[1]))
             # read spawnpoint
             elif type == 'spawnpoint':
                 if len(data) == 3:
@@ -137,40 +216,33 @@ def _FindUnknownAges():
         _AddAge('restorationlink', ageName, _Age(ageName + " (unknown age)"))
 
 
+# Public API
 def ResetAvailableLinks():
     global _AvailableLinks
     _AvailableLinks = {} # the rest will be reset by _LoadAvailableLinks - but this is necessary to make sure it re-loads the file
     _LoadAvailableLinks()
 
 
-def IsAgeAvailable(age):
+def IsAgeAvailable(ageName):
     _LoadAvailableLinks()
-    if not age in _AvailableLinks:
-        print "xLinkMgr: Age %s is not available as it's not listed at all" % age
+    if not ageName in _AvailableLinks:
+        print "xLinkMgr: Age %s is not available as it's not listed at all" % ageName
         return False
-    detect = _AvailableLinks[age].detect
-    #print 'xLinkMgr: Checking for availability: %s (%s): %s,%s' % (age, displayName, detect, linkrule)
-    if detect == 'disabled':
-        return xxConfig.isOffline() and os.path.exists('dat\\%s.age' % age)
-    if detect == 'file' or (detect.startswith('dataserver') and xxConfig.isOffline()):
-        return os.path.exists('dat\\%s.age' % age)
-    if detect.startswith('dataserver'): # online, obviously
-        return (detect == 'dataserver') or (detect == ('dataserver('+xxConfig.shardIdentifier+')'))
-    return False
+    return _AvailableLinks[ageName].IsAvailable()
 
 
-def GetInstanceName(age):
+def GetInstanceName(ageName):
     _LoadAvailableLinks()
-    if not age in _AvailableLinks:
-        return age
-    return _AvailableLinks[age].displayName
+    if not ageName in _AvailableLinks:
+        return ageName
+    return _AvailableLinks[ageName].displayName
 
 
-def GetDescription(age):
+def GetDescription(ageName):
     _LoadAvailableLinks()
-    if not age in _AvailableLinks:
+    if not ageName in _AvailableLinks:
         return ''
-    return _AvailableLinks[age].description
+    return _AvailableLinks[ageName].description
 
 
 def LinkToAge(agename, spawnpoint = None):
@@ -187,79 +259,24 @@ def LinkToAge(agename, spawnpoint = None):
 
 # If the spawnpoint is None, use the default one. If needFullInfo is True, we need the full UUID for the age.
 def GetAgeLinkStruct(agename, spawnpoint = None, needFullInfo = False):
-    # check general age availability
-    if (not IsAgeAvailable(agename)):
-        return 'The age with filename \"%s\" is not available, or does not exist at all.' % agename
-    # get age info and correct spawn point
-    ageInfo = _AvailableLinks[agename]
-    if spawnpoint == None: spawnpoint = ageInfo.defaultSpawnpoint
-    # check subage
-    partnerAge = IsSubAge(PtGetAgeName()) # the name of the parent age of the current age
-    if partnerAge == agename:
-        print "xLinkMgr: Auto-detected %s (current age) to be a sub-age of %s" % (PtGetAgeName(), agename)
-    else: # the current age is not a sub age of another age, so ignore that
-        partnerAge = ''
-    if not len(partnerAge): # ok, we are not linking to our parent age, but maybe we link to a sub age of ourselves?
-        # check whether the destination age has a partner age
-        partnerAge = IsSubAge(agename)
-        if len(partnerAge) and partnerAge != PtGetAgeName(): # indeed we are linking to a sub age, but it's someone else's sub age!
-            return 'You can not link to %s from here.' % ageInfo.displayName
-    # intialize basic age link struct
-    als = ptAgeLinkStruct()
-    ainfo = ptAgeInfoStruct()
-    ainfo.setAgeFilename(agename)
-    ainfo.setAgeInstanceName(ageInfo.displayName)
-    als.setAgeInfo(ainfo)
-    # check spawnpoint and linking rule
-    if (len(partnerAge) or IsOriginalBookAge(agename)): # we need to verify the spawn point
-        # we might need some information from the vault
-        vault = ptVault()
-        vinfo = ptAgeInfoStruct()
-        vinfo.setAgeFilename(agename)
-        vinfo = vault.getOwnedAgeLink(vinfo)
-        if len(partnerAge):
-            # either we are linking to a sub age and checked that this is indeed a sub age of the current age
-            # or fromSubAge is true, and we checked that the current age is indeed a subage of the destination age
-            if needFullInfo: return "Can't link to a sub-age with full info"
-            als.setLinkingRules(PtLinkingRules.kSubAgeBook)
-        else: # must be an original age
-            if needFullInfo:
-                if vinfo == None: return "Can't link to a not-yet registered owned age with full info"
-                als = vinfo.asAgeLinkStruct()
-            als.setLinkingRules(PtLinkingRules.kOriginalBook)
-        # now, do the spawn point thingy
-        if spawnpoint in ageInfo.spawnpoints: # we got a spawn point :)
-            als.setSpawnPoint(ptSpawnPointInfo(ageInfo.spawnpoints[spawnpoint], spawnpoint))
-            return als # done!
-        # no spawn point specified in the linking definitions
-        # let's check if that age was already linked to, and the spawn point is registered in the vault
-        if vinfo != None:
-            spoints = vinfo.getSpawnPoints()
-            for spoint in spoints:
-                if spoint.getName().lower() == spawnpoint.lower():
-                    als.setSpawnPoint(spoint) # yes, we can use it!
-                    return als # done :)
-        return 'Linking to spawn point %s in %s is not possible' % (spawnpoint, ageInfo.displayName) # no luck :(
-    elif IsBasicLinkAge(agename): # any spawn point can be used
-        als.setLinkingRules(PtLinkingRules.kBasicLink)
-        als.setSpawnPoint(ptSpawnPointInfo('LinkMgrBasicLink', spawnpoint))
-        return als
-    else:
-        return "ERROR: unable to get linkrule: %s (%s)"  % (agename, ageInfo.linkrule)
+    _LoadAvailableLinks()
+    if not agename in _AvailableLinks:
+        return 'The age with filename \"%s\" does not exist at all.' % agename
+    return _AvailableLinks[agename].GetAgeLinkStruct(spawnpoint, needFullInfo)
 
 
 def IsBasicLinkAge(agename):
     _LoadAvailableLinks()
     if not agename in _AvailableLinks:
         return False
-    return (_AvailableLinks[agename].linkrule == 'basic')
+    return _AvailableLinks[agename].IsBasicLinkAge()
 
 
 def IsOriginalBookAge(agename):
     _LoadAvailableLinks()
     if not agename in _AvailableLinks:
         return False
-    return (_AvailableLinks[agename].linkrule == 'original')
+    return _AvailableLinks[agename].IsOriginalBookAge()
 
 
 def IsSubAge(agename): # returns the name of the parent age, or an empty string
